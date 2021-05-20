5515aca8956ddd563aa1a9aefe7919e74e694f77
yueymiqua <pierre1116@gmail.com>
2021-05-19 21:59:52 -0400

    added conversation feature to chat.js file

--- components/Chat.js
+++ components/Chat.js
@@ -1,8 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 import { HeaderContext } from './HeaderContextProvider';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 function Chat(props) {
+
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([{
+      _id: 1,
+      text: 'Hello Developer',
+      createdAt: new Date(),
+      user: {
+        _id: 2,
+        name: 'React Native',
+        avatar: 'https://placeimg.com/140/140/any'
+      }
+    }])
+  },[])
+
+  const onSend = (newMessages = []) => {
+    setMessages(GiftedChat.append(messages, newMessages));
+  }
 
   return (
     <HeaderContext.Consumer>{(headerCtx) => {
@@ -11,12 +31,23 @@ function Chat(props) {
       props.navigation.setOptions({title: text});
       
       return (
-        <View style={{flex:1, justifyContent: 'center', alignItems: 'center', backgroundColor: chatBackgroundColor}}>
-          <Text>Hello Chat!</Text>
-        </View>
+        <GiftedChat
+          messages={messages}
+          onSend={(messages) => onSend(messages)}
+          user={{
+            _id:1,
+          }}
+        />
       )
     }}    
     </HeaderContext.Consumer>
   )
 }
 export default Chat;
+
+
+
+
+// <View style={{flex:1, justifyContent: 'center', alignItems: 'center', backgroundColor: chatBackgroundColor}}>
+//   <Text>Hello Chat!</Text>
+// </View>
