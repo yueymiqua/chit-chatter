cf17529ca585cdb992c52f53fbeae11e705235aa
yueymiqua <pierre1116@gmail.com>
2021-05-20 16:11:15 -0400

    changed left and right text box styling in chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,27 +1,56 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, Platform, KeyboardAvoidingView } from 'react-native';
 import { HeaderContext } from './HeaderContextProvider';
-import { GiftedChat } from 'react-native-gifted-chat';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
 function Chat(props) {
 
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    setMessages([{
-      _id: 1,
-      text: 'Hello Developer',
-      createdAt: new Date(),
-      user: {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello Developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any'
+        },
+      },
+      {
         _id: 2,
-        name: 'React Native',
-        avatar: 'https://placeimg.com/140/140/any'
-      }
-    }])
+        text: 'This is a system message',
+        CreatedAt: new Date(),
+        system: true,
+      },
+    ])
   },[])
 
   const onSend = (newMessages = []) => {
     setMessages(GiftedChat.append(messages, newMessages));
+  }
+
+  const renderBubble = (props) => {
+    return (
+      <Bubble
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: '#29bb89',
+        },
+        left: {
+          backgroundColor: '#7868e6',
+        },
+      }} 
+      textStyle={{
+        left: {
+          color: '#fff',
+        }
+      }}
+      />
+    )
   }
 
   return (
@@ -33,6 +62,7 @@ function Chat(props) {
       return (
         <View style={{flex:1, backgroundColor: chatBackgroundColor}}>
           <GiftedChat
+            renderBubble={props => renderBubble(props)}
             messages={messages}
             onSend={(messages) => onSend(messages)}
             user={{
